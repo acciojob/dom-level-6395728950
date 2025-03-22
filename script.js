7a9e551a008813a389823e444714f8e42d477ffc
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-03-22 17:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+const val  = document.getElementById("level");
+document.getElementById("x").map((item,i)=>(
+	if(item.textContent===val.textContent){
+	alert("The level of the element is:",i);
+	break;
+	}
+));
